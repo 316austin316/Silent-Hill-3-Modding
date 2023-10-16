b3d9ff128bb675bf8b03ab3e4a676b64b258639a
316austin316 <115892533+316austin316@users.noreply.github.com>
2023-10-16 18:41:53 -0500

    Updated texture extraction and gui
    so i added a new texture viewer to the gui and edited some functions on the texture extraction module, i edited the gui buttons to be easier to understand.

--- SH3X - Texture exporter and importer/gui.py
+++ SH3X - Texture exporter and importer/gui.py
@@ -4,11 +4,13 @@ import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
-from PIL import Image
+from PIL import Image, ImageTk
 import image_processing
 from image_processing import convert_bgra_to_rgba, convert_rgba_to_bgra, save_images, import_png
 import ps2_mode
 import pyglet.image.codecs.dds
+from typing import List, Tuple
+import texture_extraction  # Added import for texture_extraction
 
 global file_path  # Define file_path as a global variable
 
@@ -56,6 +58,14 @@ def analyze_textures(root):
         num_textures = len(textures)
         messagebox.showinfo("Texture Analysis", f"{num_textures} textures analyzed!")
         
+        # For each texture, visualize its palette
+        for texture in textures:
+            ps2_mode.visualize_palette(texture["palette_data"], mode="RGBA")
+
+        # Display a message box with the results
+        num_palettes = len(textures)
+        messagebox.showinfo("Palette Visualization", f"{num_palettes} palettes visualized!")
+        
         # create a new directory to store the extracted textures as unswizzled images
         output_dir = os.path.splitext(filename)[0] + "_textures"
         os.makedirs(output_dir, exist_ok=True)
@@ -67,27 +77,106 @@ def analyze_textures(root):
         ps2_mode.apply_palette_to_textures(textures, output_dir)
 
         messagebox.showinfo("Extraction Complete", f"{len(textures)} textures extracted and saved as unswizzled and colorized data to {output_dir}!")
+        
+def display_texture(textures, index):
+    texture = textures[index]
+    img = texture_extraction.convert_texture_to_image(texture)
+    window = tk.Toplevel()
+
+    # Create a frame for image display
+    image_frame = tk.Frame(window)
+    image_frame.pack(fill=tk.BOTH, expand=True)
+
+    img_tk = ImageTk.PhotoImage(img)
+    label = tk.Label(image_frame, image=img_tk)
+    label.img = img_tk  # Keep a reference to the image to prevent garbage collection
+    label.pack(fill=tk.BOTH, expand=True)
+
+    # Display texture name and size
+    texture_info = tk.Label(window, text=f"Name: {texture['filename']}, Size: {texture['width']}x{texture['height']}")
+    texture_info.pack()
+
+    # Create a frame for buttons
+    button_frame = tk.Frame(window)
+    button_frame.pack(side=tk.BOTTOM, fill=tk.X)
+
+    def show_next_texture():
+        nonlocal index
+        index = (index + 1) % len(textures)  # Cycle through textures
+        next_texture = textures[index]
+        next_img = texture_extraction.convert_texture_to_image(next_texture)
+        next_img_tk = ImageTk.PhotoImage(next_img)
+        label.config(image=next_img_tk)
+        label.img = next_img_tk  # Update the reference to prevent garbage collection
+        # Update texture info
+        texture_info.config(text=f"Name: {next_texture['filename']}, Size: {next_texture['width']}x{next_texture['height']}")
+
+    def export_texture(format):
+        filename = filedialog.asksaveasfilename(defaultextension=f".{format.lower()}", filetypes=[(f"{format} files", f"*.{format.lower()}")])
+        if filename:
+            texture = textures[index]
+            img = texture_extraction.convert_texture_to_image(texture)
+            img.save(filename, format=format)
+            messagebox.showinfo("Export Complete", f"Texture exported successfully as {format}!")
+
+    def import_texture():
+        filename = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.bmp")])
+        if filename:
+            new_img = Image.open(filename)
+            new_rgba_data = new_img.tobytes()
+            new_bgra_data = image_processing.convert_rgba_to_bgra(new_rgba_data)
+            textures[index]['data'] = new_bgra_data
+            # Optionally, refresh the displayed image
+            img_tk = ImageTk.PhotoImage(new_img)
+            label.config(image=img_tk)
+            label.img = img_tk
+            messagebox.showinfo("Import Complete", "Texture imported successfully!")
+
+    next_button = tk.Button(button_frame, text="Next", command=show_next_texture)
+    next_button.pack(side=tk.LEFT)
+
+    export_png_button = tk.Button(button_frame, text="Export as PNG", command=lambda: export_texture('PNG'))
+    export_png_button.pack(side=tk.LEFT)
+
+    export_bmp_button = tk.Button(button_frame, text="Export as BMP", command=lambda: export_texture('BMP'))
+    export_bmp_button.pack(side=tk.LEFT)
+
+    import_button = tk.Button(button_frame, text="Import", command=import_texture)
+    import_button.pack(side=tk.LEFT)
+
+    window.mainloop()
+
+
+def view_texture(root):
+    filename = filedialog.askopenfilename(parent=root, title='Select a .arc file')
+    if filename:
+        textures = texture_extraction.extract_textures(filename)
+        display_texture(textures, 0)  # Initially display the first texture
 
 def main():
     global file_path  # Use the global file_path variable
     root = Tk()
     root.title("Silent Hill 3 Texture Extractor and Importer")
-    root.geometry("400x200")
+    root.geometry("640x200")
     file_path = ""
 
-    import_button = Button(root, text="Import", command=lambda: import_file(root))
+    import_button = Button(root, text="Convert to .bgra", command=lambda: import_file(root))
     import_button.pack(side=LEFT, padx=10, pady=10)
 
-    extract_button = Button(root, text="Extract Textures", command=lambda: extract_textures(root))
+    extract_button = Button(root, text="Batch Extract Textures", command=lambda: extract_textures(root))
     extract_button.pack(side=LEFT, padx=10, pady=10)
     
     # add the reimport button
-    reimport_button = Button(root, text="Reimport Textures", command=lambda: reimport_textures(root))
+    reimport_button = Button(root, text="Batch Reimport Textures", command=lambda: reimport_textures(root))
     reimport_button.pack(side=LEFT, padx=10, pady=10)
 
     # add the ps2_mode button
     analyze_button = Button(root, text="PS2_Mode", command=lambda: analyze_textures(root))
     analyze_button.pack(side=LEFT, padx=10, pady=10)
+
+    # add the view texture button
+    view_button = Button(root, text="Texture Viewer", command=lambda: view_texture(root))
+    view_button.pack(side=LEFT, padx=10, pady=10)
 
     root.mainloop()
 
--- SH3X - Texture exporter and importer/texture_extraction.py
+++ SH3X - Texture exporter and importer/texture_extraction.py
@@ -24,73 +24,69 @@ def extract_textures(filename):
             print("Number of textures:", num_textures)
             print("Texture offset:", hex(texture_offset))
             print("Texture size:", hex(texture_size))
-            
-            if num_textures == 0:
-                # Skip the master header if there are no textures
-                f.seek(master_header_offset + 0x20)
-            else:
-                # Iterate over all textures in the section
-                for i in range(num_textures):
-                    # Read the texture header
-                    texture_header = f.read(0x20)
-                    # Check the pattern to determine the header size
-                    if texture_header[12:16] == b"\x20\x30\x00\x00":
-                        header_size = 96
-                    elif texture_header[12:16] == b"\x18\x30\x00\x00":
-                        header_size = 96
-                    elif texture_header[12:16] == b"\x08\x30\x00\x00":
-                        header_size = 96
-                    elif texture_header[12:16] == b"\x18\x50\x00\x00":
-                        header_size = 128
-                    elif texture_header[12:16] == b"\x20\x50\x00\x00":
-                        header_size = 128
-                    elif texture_header[12:16] == b"\x08\x20\x00\x00":
-                        header_size = 80
-                    else:
-                        header_size = 80
-                    # Calculate the offset of the texture header
-                    header_offset = f.tell() - 0x20
-                    # Read the remaining bytes of the texture header
-                    texture_header += f.read(header_size - 0x20)
-                    
-                    # print the texture header values and offset
-                    print(f"Texture header for texture {i}: {texture_header}")
-                    print(f"  Width: {struct.unpack('<H', texture_header[8:10])[0]}")
-                    print(f"  Height: {struct.unpack('<H', texture_header[10:12])[0]}")
-                    print(f"  Data size: {struct.unpack('<I', texture_header[16:20])[0]}")
-                    print(f"  Total size: {struct.unpack('<I', texture_header[20:24])[0]}")
 
-                    # Parse the values from the texture header
-                    width = struct.unpack("<H", texture_header[8:10])[0]
-                    height = struct.unpack("<H", texture_header[10:12])[0]
-                    data_size = struct.unpack("<I", texture_header[16:20])[0]
-                    total_size = struct.unpack("<I", texture_header[20:24])[0]
-                    data_offset = struct.unpack("<I", texture_header[4:8])[0]
+            # Iterate over all textures in the section
+            for i in range(num_textures):
+                # Read the texture header
+                texture_header = f.read(0x20)
+                # Check the pattern to determine the header size
+                if texture_header[12:16] == b"\x20\x30\x00\x00":
+                    header_size = 96
+                elif texture_header[12:16] == b"\x18\x30\x00\x00":
+                    header_size = 96
+                elif texture_header[12:16] == b"\x08\x30\x00\x00":
+                    header_size = 96
+                elif texture_header[12:16] == b"\x18\x50\x00\x00":
+                    header_size = 128
+                elif texture_header[12:16] == b"\x20\x50\x00\x00":
+                    header_size = 128
+                elif texture_header[12:16] == b"\x08\x20\x00\x00":
+                    header_size = 80
+                else:
+                    header_size = 80
+                # Calculate the offset of the texture header
+                header_offset = f.tell() - 0x20
+                # Read the remaining bytes of the texture header
+                texture_header += f.read(header_size - 0x20)
+                
+                # print the texture header values and offset
+                print(f"Texture header for texture {i}: {texture_header}")
+                print(f"  Width: {struct.unpack('<H', texture_header[8:10])[0]}")
+                print(f"  Height: {struct.unpack('<H', texture_header[10:12])[0]}")
+                print(f"  Data size: {struct.unpack('<I', texture_header[16:20])[0]}")
+                print(f"  Total size: {struct.unpack('<I', texture_header[20:24])[0]}")
 
-                    # Read the texture data
-                    texture_data_offset = f.tell()
-                    f.seek(texture_data_offset)
-                    texture_data = f.read(data_size)
-                    # Create the output directory if it doesn't exist
-                    if not os.path.exists("output"):
-                        os.mkdir("output")
-                    # Write the texture data to a separate file
-                    with open(f"output/texture_{texture_data_offset:x}.bin", "wb") as texture_file:
-                        texture_file.write(texture_data)
-                    # Move the file pointer to the start of the next texture header
-                    f.seek(texture_data_offset + data_size)
-                    # Add the texture information to the list of textures
-                    texture = {
-                        "index": i,
-                        "width": width,
-                        "height": height,
-                        "data_size": data_size,
-                        "data": texture_data,
-                        "offset": texture_data_offset,
-                        "total_size": total_size,
-                        "filename": f"texture_{texture_data_offset:x}.bin"
-                    }
-                    textures.append(texture)
+                # Parse the values from the texture header
+                width = struct.unpack("<H", texture_header[8:10])[0]
+                height = struct.unpack("<H", texture_header[10:12])[0]
+                data_size = struct.unpack("<I", texture_header[16:20])[0]
+                total_size = struct.unpack("<I", texture_header[20:24])[0]
+                data_offset = struct.unpack("<I", texture_header[4:8])[0]
+
+                # Read the texture data
+                texture_data_offset = f.tell()
+                f.seek(texture_data_offset)
+                texture_data = f.read(data_size)
+                # Create the output directory if it doesn't exist
+                if not os.path.exists("output"):
+                    os.mkdir("output")
+                # Write the texture data to a separate file
+                with open(f"output/texture_{texture_data_offset:x}.bin", "wb") as texture_file:
+                    texture_file.write(texture_data)
+                # Move the file pointer to the start of the next texture header
+                f.seek(texture_data_offset + data_size)
+                # Add the texture information to the list of textures
+                texture = {
+                    "index": i,
+                    "width": width,
+                    "height": height,
+                    "data_size": data_size,
+                    "data": texture_data,
+                    "offset": texture_data_offset,
+                    "total_size": total_size,
+                    "filename": f"texture_{texture_data_offset:x}.bin"
+                }
+                textures.append(texture)
             # Find the next master header
             master_header_offset = data.find(master_header_pattern, master_header_offset + 1)
             if master_header_offset == -1:
@@ -98,21 +94,33 @@ def extract_textures(filename):
 
     return textures
     
+def convert_texture_to_image(texture):
+    texture_data = texture["data"]
+    width = texture["width"]
+    height = texture["height"]
+
+    # Convert BGRA data to RGBA
+    rgba_data = convert_bgra_to_rgba(texture_data)
+
+    # Convert RGBA data to a Pillow Image object
+    img = Image.frombytes("RGBA", (width, height), rgba_data)
+    return img
+
+def save_image_to_disk(img, filename, output_dir):
+    img_path = os.path.join(output_dir, f"{filename}.png")
+    img.save(img_path)
+
+
 def convert_textures_to_png(textures, output_dir):
-    # Iterate over all extracted textures and convert to PNG
+    # Iterate over all extracted textures
     for i, texture in enumerate(textures):
-        texture_data = texture["data"]
-        width = texture["width"]
-        height = texture["height"]
         filename = texture["filename"]
-
-        # Convert BGRA data to RGBA
-        rgba_data = convert_bgra_to_rgba(texture_data)
-
-        # Convert RGBA data to PNG and save
-        img = Image.frombytes("RGBA", (width, height), rgba_data)
-        img_path = os.path.join(output_dir, f"{filename}.png")
-        img.save(img_path)
+        
+        # Convert texture to a Pillow Image object
+        img = convert_texture_to_image(texture)
+        
+        # Save the Pillow Image object to disk
+        save_image_to_disk(img, filename, output_dir)
 
     print(f"All textures converted to PNG and saved to: {output_dir}")
 
